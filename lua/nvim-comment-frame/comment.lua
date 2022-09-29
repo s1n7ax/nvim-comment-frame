1c6379300d7ce306657481c4b7a51d72f387ae89
s1n7ax <srineshnisala@gmail.com>
2022-09-29 23:26:45 +0530

    fix: fill char fills spaces in start and end strings

--- lua/nvim-comment-frame/comment.lua
+++ lua/nvim-comment-frame/comment.lua
@@ -1,5 +1,5 @@
-local Assert = require('nvim-comment-frame.assertion')
-local Util = require('nvim-comment-frame.util')
+local Assert = require("nvim-comment-frame.assertion")
+local Util = require("nvim-comment-frame.util")
 
 local Comment = {}
 
@@ -14,9 +14,6 @@ function Comment:new(opt)
 	self.frame_width = opt.frame_width
 	self.line_wrap_len = opt.line_wrap_len
 	self.indent_str = opt.indent_str
-
-	self.COMMENT_LINE_FORMAT = '%%s%%%is%%s'
-	self.TEXT_LINE_FORMAT = '%%s%%%is%%s%%%is%%s'
 
 	return self
 end
@@ -41,10 +38,7 @@ function Comment:validate_config(opt)
 
 	local padding = opt.frame_width - (opt.start_str:len() + opt.end_str:len())
 
-	Assert.is_true(
-		opt.line_wrap_len <= padding,
-		"line_wrap_len should be less than or equal to " .. padding
-	)
+	Assert.is_true(opt.line_wrap_len <= padding, "line_wrap_len should be less than or equal to " .. padding)
 end
 
 -- Returns the lines of comment frame
@@ -66,37 +60,24 @@ end
 
 -- Returns comment line for a given text
 function Comment:get_text_line(text)
-	local padding = self.frame_width - (
-		self.start_str:len() + self.end_str:len() + text:len()
-	)
+	local padding = self.frame_width - (self.start_str:len() + self.end_str:len() + text:len())
 
 	local left_padding = math.floor(padding / 2)
 	local right_padding = padding - left_padding
 
-	local line = self.TEXT_LINE_FORMAT
-		:format(left_padding, right_padding)
-		:format(
-			self.start_str,
-			'',
-			text,
-			'',
-			self.end_str
-		)
-	return self.indent_str .. line
+	return self.indent_str
+		.. self.start_str
+		.. string.rep(" ", left_padding)
+		.. text
+		.. string.rep(" ", right_padding)
+		.. self.end_str
 end
 
 -- Returns border of the comment frame
 function Comment:get_border_line()
-	local padding_len = self.frame_width - (self.start_str:len() + self.end_str:len())
-	local comment, _ =  self.COMMENT_LINE_FORMAT
-		:format(padding_len)
-		:format(
-			self.start_str,
-			'',
-			self.end_str
-		):gsub(' ', self.fill_char)
+	local fill_len = self.frame_width - (self.start_str:len() + self.end_str:len())
 
-	return self.indent_str .. comment
+	return self.indent_str .. self.start_str .. string.rep(self.fill_char, fill_len) .. self.end_str
 end
 
 return Comment
